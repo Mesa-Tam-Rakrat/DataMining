d75693ed8a1706a7ed811571d7a19397c42fa74a
Mesa-Tam-Rakrat <366436241002-st@rmutsb.ac.th>
2025-07-25 11:13:07 +0700

    First commit

---
+++ Basic_Python/test.py
@@ -0,0 +1,12 @@
+#create dictionaries key : vaiue
+group_idm = {
+    1 : 'film',
+    2 : 'tam',
+    3 : 'kade',
+    4 : 'nice',
+    5 : 'pemai',
+}
+group_idm[6] = 'tum'
+# print(group_idm)
+
+print(group_idm[4])
